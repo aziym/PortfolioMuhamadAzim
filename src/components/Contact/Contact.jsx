abfdb86a3998f624c87a8a990a6f09f52f801152
aziym <azim.bakri2002@gmail.com>
2025-01-18 01:30:56 +0800

    Update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ export const Contact = () => {
   return (
     <footer id="contact" className={styles.container}>
       <div className={styles.text}>
-        <p>Contact me</p>
+        <h3>Contact me</h3>
         <p>Feel free to reach out!</p>
       </div>
       <ul className={styles.links}>
